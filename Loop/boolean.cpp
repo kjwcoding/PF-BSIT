9ad3a62f4e86fc9de6f1e7206e080674ec221e29
Kashif Hussain <kh9732494@gmail.com>
2024-07-02 14:19:42 +0500

    Loop

---
+++ Loop/boolean.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    if (("true") && ("false"))
+    {
+        cout << (2 > 1);
+        // cout << "true";
+    }
+    else
+        cout << (2 < 1);
+    // cout << "false";
+
+    return 0;
+}
